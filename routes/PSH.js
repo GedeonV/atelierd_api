54dc7afa67bf5c4d8381378fec92358c1fd93d36
Arthur <zinni.arthur@gmail.com>
2022-03-24 17:30:00 +0100

    upload csv

--- routes/PSH.js
+++ routes/PSH.js
@@ -7,6 +7,7 @@ psh.use(cors());
 
 psh.get("/", PSHController.psh_get_all);
 psh.post("/create", PSHController.psh_create);
+psh.post("/upload", PSHController.psh_upload);
 psh.get("/psh/:_id", PSHController.psh_get_id);
 psh.delete("/:_id", PSHController.psh_delete);
 
